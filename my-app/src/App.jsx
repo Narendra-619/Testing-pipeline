71562d7521782bcd4d07c310f3329414354bb85f
Narendra-619 <kknarendra619@gmail.com>
2026-07-22 00:56:04 +0530

    made some changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <h1>🚀 Jenkins CI/CD Pipeline Test</h1>
       <p>If you can see this, the pipeline worked successfully in ohio!</p>
-      <button onClick={() => alert("Pipeline Test Successful and check succesful and integrating it with dockerous!")}>
+      <button onClick={() => alert("Pipeline Test Successful and check succesful and integrating it with dockerous again!")}>
         Click Me
       </button>
     </div>
