584d24febe012ddc153461d510c23d05afee9d3d
Narendra-619 <kknarendra619@gmail.com>
2026-07-21 23:26:20 +0530

    made some changes

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🚀 Jenkins CI/CD Pipeline Test</h1>
+      <p>If you can see this, the pipeline worked successfully!</p>
+      <button onClick={() => alert("Pipeline Test Successful!")}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
